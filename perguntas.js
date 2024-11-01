139076901d5653fe82eeb11c49952f9331017d46
carlsx <161041736+carlsx@users.noreply.github.com>
2024-11-01 11:16:58 -0300

    projeto
    y545y5g

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Fórmula 1',
+    'O que é Fórmula 1?',
+    'Fórmula 1 é a categoria máxima do automobilismo, composta por corridas de monovolumes em circuitos fechados.'
+)
+
+criaCartao(
+    'Fórmula 1',
+    'Qual é a principal competição da Fórmula 1?',
+    'A principal competição da Fórmula 1 é o Campeonato Mundial de Pilotos e o Campeonato Mundial de Construtores.'
+)
+
+criaCartao(
+    'Fórmula 1',
+    'Quais são os principais componentes de um carro de Fórmula 1?',
+    'Os principais componentes incluem chassi, motor, aerodinâmica, suspensão e pneus.'
+)
+
+criaCartao(
+    'Fórmula 1',
+    'Quem é considerado o maior piloto de Fórmula 1?',
+    'Os pilotos mais frequentemente mencionados como os maiores incluem Michael Schumacher e Lewis Hamilton, devido aos seus recordes de vitórias e títulos.'
+)
